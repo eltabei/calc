7777df0889b2b13d60235e373306b490ebef1b3c
Eltabei Mohamed <eltabei.m@gmail.com>
2022-07-09 22:23:43 +0200

    fixed repressing operators behavior

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // TODO
 // fix bug that happens sometimes when mixing using keyboard and mouse in
 // one operation
-// add multi operator pressing support
+// handle repeated pressing of equal (emulate Windows calc behavior)
 
-const CalcScreenDefault = "0.";
-const Operators = ["+", "-", "*", "/"];
+const CalcScreenDefault = "0";
+const CalcOperators = ["+", "-", "*", "/"];
+const Equal = "=";
 const tb = document.getElementById("calcScreen");
+const eqn = document.getElementsByClassName("eqn")[0];
 const allBtns = document.getElementsByClassName("btn");
 const digitBtns = document.getElementsByClassName("digit");
 const opBtns = document.getElementsByClassName("op");
@@ -14,9 +16,11 @@ const eqBtn = document.getElementsByClassName("eq")[0];
 const clrBtn = document.getElementsByClassName("clr")[0];
 const delBtn = document.getElementsByClassName("del")[0];
 
-let op = null;
+// array of operators
+let operators = [];
+// last calculator key pressed
 let lastKeyDown = null;
-// contains parts of equation: operands and operators
+// array of operands (numbers)
 let operands = [];
 
 // add event listeners to all digit buttons
@@ -25,39 +29,58 @@ for (const btn of digitBtns) {
 }
 // add event listeners to all op buttons
 for (const btn of opBtns) {
-  btn.addEventListener("click", () => opPressed(btn));
+  btn.addEventListener("click", () => operatorPressed(btn));
 }
-
-eqBtn.addEventListener("click", eqPressed);
+// add event listeners to rest of buttons
+eqBtn.addEventListener("click", equalPressed);
 clrBtn.addEventListener("click", clearPressed);
 delBtn.addEventListener("click", delPressed);
 
 
 function digitPressed(btn) {
-  if (lastKeyDown === null || Operators.includes(lastKeyDown)) {
+  // if this is the very first key to be pressed
+  // or if an operator was just pressed
+  if (lastKeyDown === null || CalcOperators.includes(lastKeyDown)) {
     tb.value = btn.value;
   } else {
     tb.value += btn.value;
   }
   lastKeyDown = btn.value;
-  //console.log(lastKeyDown);
 }
 
 
-function opPressed(btn) {
-  //console.log("opPressed: " + btn.value);
-  if (operands.length == 2) {
-    eqPressed();
+function operatorPressed(btn) {
+  // if 2 operators pressed in a row, ignore last one
+  if (CalcOperators.includes(lastKeyDown)) {
+    operators.pop();
   }
-  op = btn.value;
+  operators.push(btn.value);
   operands.push(tb.value);
+  if (operators.length == 2) {
+    solve();
+    operands = [tb.value];
+    operators = [btn.value];
+    //operators.shift();
+  }
   lastKeyDown = btn.value;
+  updateEquation(false);
 }
 
-function eqPressed() {
+
+function equalPressed() {
   operands.push(tb.value);
-  console.log("eqPressed", operands, op);
-  switch (op) {
+  console.log("eqPressed", operands, operators);
+  updateEquation();
+  solve();
+  operands = [];
+  operators.shift();
+  lastKeyDown = null;
+  console.log(operands, operators);
+}
+
+
+function solve() {
+    switch (operators[0]) {
     case "+":
       tb.value = parseFloat(operands[0]) + parseFloat(operands[1]);
       break;
@@ -74,17 +97,32 @@ function eqPressed() {
       //alert("Error: unknown operator");
       tb.value = CalcScreenDefault;
   }
-  operands = [];
-  lastKeyDown = null;
-  op = null;
 }
+
+
+function updateEquation(byEqualKey=true) {
+  if (operands.length == 0) {
+    eqn.textContent = "";
+  } else if (operands.length == 1) {
+    eqn.textContent = `${operands[0]} ${operators[0]}`;
+  } else {
+    if (byEqualKey) {
+      eqn.textContent = `${operands[0]} ${operators[0]} ${operands[1]} = `;
+    } else {
+      eqn.textContent = `${tb.value} ${operators[operators.length - 1]}`;
+    }
+  }
+}
+
 
 function clearPressed() {
   tb.value = CalcScreenDefault;
-  op = null;
+  operators = [];
   operands = [];
   lastKeyDown = null;
+  updateEquation();
 }
+
 
 function delPressed() {
   if (tb.value == CalcScreenDefault) {
@@ -96,9 +134,10 @@ function delPressed() {
   }
 }
 
+
 document.onkeydown = function(e) {
   let btn = document.querySelector("div.btnsContainer > button[value='" + e.key + "']");
-  console.log("Keydown: " + e.key);
+  //console.log("Keydown: " + e.key);
   switch (e.key) {
     case "0":
     case "1":
@@ -121,7 +160,6 @@ document.onkeydown = function(e) {
       break;
     case "Enter":
     case "=":
-      let eqBtn = document.querySelector("div.btnsContainer > button[value='=']");
       eqBtn.click();
       break;
     case "Backspace":
